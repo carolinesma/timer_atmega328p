9be27445d46b122ed2f0d40cb6802c3d1d542bde
carolinesma <sma.caroline@ymail.com>
2022-04-25 21:03:04 -0300

    Update timer.ino

--- timer/timer.ino
+++ timer/timer.ino
@@ -8,16 +8,24 @@
 
 volatile unsigned long count_millis;
 
+/* Uso do Timer2 para contar 1ms
+
+ts = (TOP + 1) x preescaler/ Fosc
+ts = (255 + 1 - 56) x 8/16mHz
+ts = 1000s
+
+*/
 void inicializa_timer(void){
   
-  TCCR2A = 0x00;
-  TCCR2B |= (1<<CS21);
-  TCNT2 = 56;
-  TIMSK2 |= (1<<TOIE2);
-  timer_millis = 0;
+  TCCR2A = 0x00;            // Modo de operação normal
+  TCCR2B |= (1<<CS21);      // Set Preescaler de 8 bits
+  TCNT2 = 56;               // Inicializa o contador do Timer com 56 para que conte apenas 200 vezes
+  TIMSK2 |= (1<<TOIE2);     // Ativa a interrupção por overflow
+  count_millis = 0;         // inicializa contador
  
 }
 
+/* Chamada de Interurpçação para contabilizar os ms a cada estouro*/
 ISR (TIMER2_OVF_vect)
 {      
    TCNT2   =    56;
@@ -25,33 +33,31 @@ ISR (TIMER2_OVF_vect)
    
 }
 
+/* Função para retornar  o valor contabilizado na hora da requisição*/
 unsigned long whatsTime(void){
   unsigned long aux_millis;
 
-  ATOMIC_BLOCK(ATOMIC_FORCEON) {
+  ATOMIC_BLOCK(ATOMIC_FORCEON) { //desabilita as interrupções gerais
     aux_millis = count_millis;
   }
   return aux_millis;
 }
 
 void setup_init(void){
-  Serial.begin(9600);
   inicializa_timer();
 }
 
 
 int main( void )
 {
-  cli();  
+  cli();    // desabilita interrupções
   setup_init();
-  unsigned long timer;
-  sei();
-
   
-  timer = whatsTime();
-  Serial.println(timer);
-
+  /*
+  ...
+  */
   
+  sei();    // habilita interrupções
   
 
 }
